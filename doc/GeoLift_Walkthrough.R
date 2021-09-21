3de391106da5bd4b2e1146b3f765560eeff9a9c3
ArturoEsquerra <arturo.esquerra@berkeley.edu>
2021-09-21 17:32:05 -0500

    Adding additional comments

--- doc/GeoLift_Walkthrough.R
+++ doc/GeoLift_Walkthrough.R
@@ -1,9 +1,14 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 ## ---- include = FALSE---------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>",
-  fig.width = 6, 
-  fig.height = 4, 
+  fig.width = 6,
+  fig.height = 4,
   fig.align = "center"
 )
 

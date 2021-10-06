d7f37e1b1a6e6cd3c4d2580575a829781d670d57
ArturoEsquerra <arturo.esquerra@berkeley.edu>
2021-10-06 11:25:37 -0500

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -12,7 +12,7 @@
 (module.exports = {
   title: 'GeoLift',
   tagline: 'The solution to calculate Lift at a geo-level',
-  url: 'https://facebookincubator.github.io/GeoLift',
+  url: 'https://facebookincubator.github.io/',
   baseUrl: '/GeoLift/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',

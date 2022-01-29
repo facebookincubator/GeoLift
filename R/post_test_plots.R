542de5eedf15d8917e7be39fff1fcf91a5fc6110
Nicolas Cruces <nicocru@fb.com>
2022-01-29 13:56:59 -0300

    split plots into two

--- R/post_test_plots.R
+++ R/post_test_plots.R
@@ -2,174 +2,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-# Includes function GeoPlot, plot.GeoLiftPower, plot.GeoLift, Lift.plot,
-# absolute_value.plot, cumulative_value.plot.
-
-
-#' Plotting function for Exploratory Analysis.
-#'
-#' @description
-#'
-#' \code{GeoPlot} takes a data frame used for GeoLift to generate
-#' a plot of each location's time series.
-#'
-#' @param data A data.frame containing the historical conversions by
-#' geographic unit. It requires a "locations" column with the geo name,
-#' a "Y" column with the outcome data (units), a time column with the indicator
-#' of the time period (starting at 1), and covariates.
-#' @param Y_id Name of the outcome variable (String).
-#' @param time_id Name of the time variable (String).
-#' @param location_id Name of the location variable (String).
-#' @param treatment_start Treatment start (Default = 0).
-#'
-#' @return
-#' A plot of each location's time series.
-#'
-#' @export
-GeoPlot <- function(data,
-                    Y_id = "Y",
-                    time_id = "time",
-                    location_id = "location",
-                    treatment_start = 0) {
-  if (treatment_start == 0) {
-    size_vline <- 0
-  } else {
-    size_vline <- 0.8
-  }
-
-  p <- ggplot(data, aes(y = !!sym(Y_id), x = !!sym(time_id), colour = !!sym(location_id), label = !!sym(location_id))) +
-    geom_line(show.legend = FALSE) +
-    geom_vline(xintercept = treatment_start, linetype = "dashed", size = size_vline, color = "grey35") +
-    geom_dl(aes(label = !!sym(location_id)), method = list(dl.combine("last.points"), cex = 0.8)) +
-    xlim(1, 1.15 * (max(data[[time_id]]))) +
-    ylab("") +
-    theme_minimal()
-
-  print(p)
-}
-
-
-#' Plotting function for GeoLiftPower.
-#'
-#' @description
-#'
-#' Plotting function for \code{GeoLiftPower}. The function smooths the power curve
-#' for ease of interpretation.
-#'
-#' @param x GeoLiftPower object.
-#' @param power Power level. By default 0.8.
-#' @param table Plot the table of power estimates. TRUE by default.
-#' @param actual_values Logic flag indicating whether to include in the plot
-#' the actual values in addition to the smoothed values.
-#' @param ... additional arguments
-#'
-#' @return
-#' GeoLiftPower plot.
-#'
-#' @export
-plot.GeoLiftPower <- function(x,
-                              power = 0.8,
-                              table = TRUE,
-                              actual_values = FALSE,
-                              ...) {
-  if (!inherits(x, "GeoLiftPower")) {
-    stop("object must be class GeoLiftPower")
-  }
-
-  treatment_periods <- unique(x$duration)
-  lift <- unique(x$lift)
-
-  # NewChange: Standardize Plots
-  PowerPlot <- x %>%
-    dplyr::group_by(duration, lift) %>%
-    # dplyr::mutate(power = 1 - pvalue) %>%
-    # dplyr::summarise(power = mean(power))
-    dplyr::summarise(power = mean(pow))
-
-  spending <- x %>%
-    dplyr::group_by(duration, lift) %>%
-    dplyr::summarize(inv = mean(investment))
-
-  if (table == TRUE) {
-    print(as.data.frame(PowerPlot))
-  }
-
-  if (actual_values == FALSE) {
-    if (sum(spending$inv > 0)) {
-      for (dur in unique(PowerPlot$duration)) {
-        PowerPlot_aux <- as.data.frame(PowerPlot %>% dplyr::filter(duration == dur))
-
-        CostPerLift <- as.numeric(x %>%
-          dplyr::filter(duration == dur, lift > 0) %>%
-          dplyr::mutate(AvgCost = investment / lift) %>%
-          dplyr::summarise(mean(AvgCost)))
-
-        PowerPlot_graph <- ggplot(PowerPlot_aux, aes(x = lift, y = power)) +
-          geom_smooth(formula = y ~ x, color = "indianred3", method = "loess", se = FALSE) +
-          scale_x_continuous(sec.axis = sec_axis(~ . * CostPerLift, name = "Estimated Investment")) +
-          ylim(0, 1) +
-          geom_hline(yintercept = 0.8, linetype = "dashed", color = "grey") +
-          labs(title = paste0("Treatment Periods: ", dur), x = "Effect Size", y = "Power") +
-          theme_minimal() +
-          theme(plot.title = element_text(hjust = 0.5))
-
-        plot(PowerPlot_graph)
-      }
-    } else {
-      for (dur in unique(PowerPlot$duration)) {
-        PowerPlot_aux <- as.data.frame(PowerPlot %>% dplyr::filter(duration == dur))
-
-        PowerPlot_graph <- ggplot(PowerPlot_aux, aes(x = lift, y = power)) +
-          geom_smooth(formula = y ~ x, color = "indianred3", method = "loess", se = FALSE) +
-          ylim(0, 1) +
-          geom_hline(yintercept = 0.8, linetype = "dashed", color = "grey") +
-          labs(title = paste0("Treatment Periods: ", dur), x = "Effect Size", y = "Power") +
-          theme_minimal() +
-          theme(plot.title = element_text(hjust = 0.5))
-
-        plot(PowerPlot_graph)
-      }
-    }
-  } else if (actual_values == TRUE) {
-    if (sum(spending$inv > 0)) {
-      for (dur in unique(PowerPlot$duration)) {
-        PowerPlot_aux <- as.data.frame(PowerPlot %>% dplyr::filter(duration == dur))
-
-        CostPerLift <- as.numeric(x %>%
-          dplyr::filter(duration == dur, lift > 0) %>%
-          dplyr::mutate(AvgCost = investment / lift) %>%
-          dplyr::summarise(mean(AvgCost)))
-
-        PowerPlot_graph <- ggplot(PowerPlot_aux, aes(x = lift, y = power)) +
-          geom_smooth(formula = y ~ x, color = "indianred3", method = "loess", se = FALSE) +
-          geom_line(color = "gray80", size = 0.62, alpha = 0.8) +
-          scale_x_continuous(sec.axis = sec_axis(~ . * CostPerLift, name = "Estimated Investment")) +
-          ylim(0, 1) +
-          geom_hline(yintercept = 0.8, linetype = "dashed", color = "grey") +
-          labs(title = paste0("Treatment Periods: ", dur), x = "Effect Size", y = "Power") +
-          theme_minimal() +
-          theme(plot.title = element_text(hjust = 0.5))
-
-        plot(PowerPlot_graph)
-      }
-    } else {
-      for (dur in unique(PowerPlot$duration)) {
-        PowerPlot_aux <- as.data.frame(PowerPlot %>% dplyr::filter(duration == dur))
-
-        PowerPlot_graph <- ggplot(PowerPlot_aux, aes(x = lift, y = power)) +
-          geom_smooth(formula = y ~ x, color = "indianred3", method = "loess", se = FALSE) +
-          geom_line(color = "gray80", size = 0.62, alpha = 0.8) +
-          ylim(0, 1) +
-          geom_hline(yintercept = 0.8, linetype = "dashed", color = "grey") +
-          labs(title = paste0("Treatment Periods: ", dur), x = "Effect Size", y = "Power") +
-          theme_minimal() +
-          theme(plot.title = element_text(hjust = 0.5))
-
-        plot(PowerPlot_graph)
-      }
-    }
-  }
-}
+# Includes function plot.GeoLift, Lift.plot, absolute_value.plot, 
+# cumulative_value.plot.
 
 
 #' Plot for GeoLift.
@@ -208,7 +42,7 @@ plot.GeoLift <- function(x,
   if (!inherits(x, "GeoLift")) {
     stop("object must be class GeoLift")
   }
-
+  
   if (type == "TreatmentSchedule") {
     panelView(Y ~ D, data = x$data, index = c("location", "time"), pre.post = TRUE)
   } else if (tolower(type) %in% c("att", "incrementality")) {
@@ -278,7 +112,7 @@ Lift.plot <- function(GeoLift,
     )
   ) * nrow(GeoLift$test_id)
   colnames(treatment_obs) <- c("t_obs")
-
+  
   q_treatment_locations <- length(GeoLift$test_id$name)
   df <- data.frame(
     t_obs = treatment_obs$t_obs,
@@ -287,7 +121,7 @@ Lift.plot <- function(GeoLift,
     c_obs_upper_bound = treatment_obs$t_obs - GeoLift$summary$att$lower_bound * q_treatment_locations,
     Time = 1:length(treatment_obs$t_obs)
   )
-
+  
   if (!is.null(treatment_end_date)) {
     plot_dates <- get_date_from_test_periods(GeoLift, treatment_end_date, frequency = frequency)
     df$Time <- plot_dates$date_vector
@@ -300,7 +134,7 @@ Lift.plot <- function(GeoLift,
       treatment_end = GeoLift$TreatmentEnd
     )
   }
-
+  
   if (!is.null(plot_start_date)) {
     if (is.null(treatment_end_date)) {
       stop("If you want to filter your dataset on a date, please specify treatment_end_date param so periods are converted to dates.")
@@ -308,13 +142,13 @@ Lift.plot <- function(GeoLift,
       df <- df[df$Time >= plot_start_date, ]
     }
   }
-
+  
   if (nchar(title) == 0) {
     title <- "Observations per Timestamp and Test Group"
   }
-
+  
   colors <- c("Treatment" = "#52854C", "Control" = "#7030A0")
-
+  
   ggplot(df, aes(x = Time)) +
     geom_line(
       aes(y = c_obs, color = "Control"),
@@ -375,7 +209,7 @@ absolute_value.plot <- function(GeoLift,
                                 ...) {
   df <- GeoLift$summary$att
   df <- df[, c("Time", "Estimate", "lower_bound", "upper_bound")]
-
+  
   if (tolower(plot_type) == "incrementality") {
     q_treatment_locations <- length(GeoLift$test_id$name)
     df$Estimate <- df$Estimate * q_treatment_locations
@@ -399,7 +233,7 @@ absolute_value.plot <- function(GeoLift,
   } else {
     stop("Please specify which plot type you would like: ATT or Incrementality.")
   }
-
+  
   if (!is.null(treatment_end_date)) {
     plot_dates <- get_date_from_test_periods(GeoLift, treatment_end_date, frequency = frequency)
     df$Time <- plot_dates$date_vector
@@ -419,7 +253,7 @@ absolute_value.plot <- function(GeoLift,
       df <- df[df$Time >= plot_start_date, ]
     }
   }
-
+  
   ggplot(df, aes(x = Time, y = Estimate)) +
     geom_line(linetype = "dashed", color = "#373472", size = 0.75) +
     geom_vline(xintercept = plot_dates$treatment_start, linetype = "dashed", alpha = 0.3) +
@@ -490,7 +324,7 @@ cumulative_value.plot <- function(data,
     time_id = time_id,
     Y_id = Y_id
   )
-
+  
   if (nchar(title) == 0) {
     title <- "Accumulated Incremental Value"
   }

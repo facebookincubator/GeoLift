5d26c059e9bc19143bb4451d2935d41bd600bb47
Nicolas Cruces <nicocru@fb.com>
2022-03-25 16:37:56 -0300

    fixed In if (ncol(similarity_matrix) < treatment_sizes) { : warning msg

--- R/auxiliary.R
+++ R/auxiliary.R
@@ -144,10 +144,10 @@ limit_test_markets <- function(similarity_matrix,
     }
   }
 
-  if (ncol(similarity_matrix) < treatment_sizes) {
-    stop("\nParameter N is larger than markets after exclusion.  Consider excluding less markets.")
+  if (ncol(similarity_matrix) < min(treatment_sizes)) {
+    stop("\Minimum N param is larger than markets after exclusion.  Consider excluding less markets.")
   }
-
+  
   return(treatment_sizes)
 }
 

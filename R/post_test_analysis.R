9a4c8a400da40b63391ec05014474ff75398160e
Nicolas Cruces <nicocru@fb.com>
2022-05-31 19:01:32 -0300

    edited top docs

--- R/post_test_analysis.R
+++ R/post_test_analysis.R
@@ -2,7 +2,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-# Includes function GeoLift, cumulative_lift, summary.GeoLift,
+# Includes function GeoLift, print.GeoLift, cumulative_lift, summary.GeoLift,
 # print.summary.GeoLift.
 
 
